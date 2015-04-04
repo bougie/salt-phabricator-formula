fc363f4d5ca3999a7a3eb622ee1b0db95314f0fd
bougie <bougie@appartland.eu>
2015-04-04 23:49:17 +0200

    add saltstack phabricator_config state (only managed)

---
+++ _states/phabricator_config.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+'''
+Support for phabricator configuration
+'''
+
+def managed(name, value, **kwargs):
+    ret = {'name': name,
+           'changes': {},
+           'comment': '',
+           'result': False}
+
+    old_value = __salt__['phabricator_config.get_option'](name)
+
+    if value != old_value:
+        if not __opts__['test']:
+            __salt__['phabricator_config.set_option'](name, value)
+            ret['result'] = True
+        else:
+            ret['result'] = None
+
+        ret['changes'] = {'new': value, 'old': old_value}
+        ret['comment'] = 'New value was set for option %s' % (name,)
+    else:
+        ret['comment'] = 'The option %s has the right value' % (name,)
+        ret['result'] = True
+
+    return ret
+
+
+if __name__ == "__main__":
+    __salt__ = ''
+    __opts__ = ''
+
+    import sys
+    sys.exit(0)
